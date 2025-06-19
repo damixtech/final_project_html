472a6e7e2f3cc95edf35609f03eb621a12df2564
Damian AR <damitech@tutamail.com>
2025-06-19 11:14:01 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,30 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-
-
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", // Rutas relativas para GitHub Pages
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+  base: "/page_final_project_html/",
+
+  build: {
+    rollupOptions: {
+      input: [
+        "index.html",
+        "404.html",
+        "aviso_legal.html",
+        "blog.html",
+        "contacto.html",
+        "curso_css.html",
+        "curso_html.html",
+        "cursos.html",
+        "login.html",
+        "post_css.html",
+        "post_html.html",
+        "quienes_somos.html",
+        "registro.html",
+      ],
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
     },
   },
 });
